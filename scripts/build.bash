fa636cda10234862dd1b09a24b71e1a16db5d5f3
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-05-20 07:47:12 +0000

    ci: 自動リリースを設定

---
+++ scripts/build.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+cd "${BASH_SOURCE[0]%/*}"/..
+
+env_name=nucleo_f303k8
+
+mkdir -p build
+for id in {20..29}; do
+  pio run -e $env_name
+  sed -i -e "/int can_id/s/[0-9]\+/${id}/" src/main.cpp
+  mv ".pio/build/${env_name}/firmware.bin" build/solenoidboard_firm_${id}.bin
+done
