ad443e1620aef4b7ed2da13cdfed8959677c3f7b
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-06-27 18:20:02 +0900

    fix(ci): idのズレを修正

--- scripts/build.bash
+++ scripts/build.bash
@@ -6,7 +6,7 @@ env_name=nucleo_f303k8
 
 mkdir -p build
 for id in {20..29}; do
-  pio run -e $env_name
   sed -i -e "/int can_id/s/[0-9]\+/${id}/" src/main.cpp
+  pio run -e $env_name
   mv ".pio/build/${env_name}/firmware.bin" build/solenoidboard_firm_${id}.bin
 done
